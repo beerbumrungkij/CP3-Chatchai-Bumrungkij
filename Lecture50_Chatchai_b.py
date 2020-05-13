bba769f46600c96570eeda45763a418bcb06e125
beerbumrungkij <34678332+beerbumrungkij@users.noreply.github.com>
2020-05-13 22:11:11 +0700

    Add files via upload

---
+++ Lecture50_Chatchai_b.py
@@ -0,0 +1,15 @@
+def add(x,y):
+    print('add      = ', x+y)
+def substrac(x,y):
+    print('substrac = ', x-y)
+def multiple(x,y):
+    print('multiple = ', x*y)
+def divide(x,y):
+    print('divide   = ', x/y)
+
+a = int(input("A : enter number :"))
+b = int(input("B : enter number :"))
+add(a,b)
+substrac(a,b)
+multiple(a,b)
+divide(a,b)
